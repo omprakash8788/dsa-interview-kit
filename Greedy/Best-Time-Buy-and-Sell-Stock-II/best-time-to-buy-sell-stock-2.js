eaab98fd38c4e763ffe8b14d9ebe53820241201f
omprakash8788 <omprakashkumar552@gmail.com>
2026-05-02 11:24:16 +0530

    best time to buy sell stock 2

---
+++ Greedy/Best-Time-Buy-and-Sell-Stock-II/best-time-to-buy-sell-stock-2.js
@@ -0,0 +1,13 @@
+let prices = [7,1,5,3,6,4];
+
+function maxProfit(prices) {
+    let profit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    return profit;
+}
+console.log(maxProfit(prices));
+
